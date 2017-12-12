13e07136a9f1c69963fe1137adf0c321f4c9ef50
Alexandre Chaintreuil <alexandre.chaintreuil@gmail.com>
2017-12-13 00:07:08 +0100

    add throttling on hashes generation

--- seald/haas/views.py
+++ seald/haas/views.py
@@ -4,6 +4,7 @@ from rest_framework import status, viewsets
 from rest_framework.authtoken.models import Token
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework.throttling import UserRateThrottle
 from rest_framework.views import APIView
 
 from haas.serializers import UserSerializer, GroupSerializer
@@ -67,6 +68,7 @@ class DummyHashView(APIView):
 
 class HashView(APIView):
     permission_classes = (IsAuthenticated,)
+    throttle_classes = (UserRateThrottle,)
 
     def post(self, request, *args, **kw):
         data = request.data.get('data', None)
--- seald/seald/settings.py
+++ seald/seald/settings.py
@@ -127,5 +127,13 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
-    )
+    ),
+    'DEFAULT_THROTTLE_CLASSES': (
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ),
+    'DEFAULT_THROTTLE_RATES': {
+        'user': '5/min',
+        'anon': '100/sec'
+    }
 }
