05fe145c716e435af69d02023f8ae37b457f8f18
Alexandre Chaintreuil <alexandre.chaintreuil@gmail.com>
2017-12-13 01:52:06 +0100

    exit debug mode

--- seald/settings.py
+++ seald/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'be2ni2y2c*k-_obayp8uw%0a$i2ctt=@%bttb$_yjb8&2ig$q$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', False)
 
 ALLOWED_HOSTS = ['*']
 
